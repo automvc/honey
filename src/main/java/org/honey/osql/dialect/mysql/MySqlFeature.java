a64a0e0ea6985ed2cc10713b4ff8574fbee3d2ba
Kingstar <honeysoft@126.com>
2018-12-27 23:21:15 +0800

    rename

--- src/main/java/org/honey/osql/dialect/mysql/MySqlFeature.java
+++ src/main/java/org/honey/osql/dialect/mysql/MySqlFeature.java
@@ -8,14 +8,14 @@ import org.bee.osql.dialect.DbFeature;
  */
 public class MySqlFeature implements DbFeature {
 
-	public String toFromSql(String sql, int from, int size) {
+	public String toPageSql(String sql, int from, int size) {
 		sql=sql.replace(";", ""); //去掉原来有的分号
 		String limitStament = " limit " + from + "," + size + ";";
 		sql += limitStament;
 		return sql;
 	}
 	
-	public String toFromSql(String sql, int size) {
+	public String toPageSql(String sql, int size) {
 		sql=sql.replace(";", ""); //去掉原来有的分号
 		String limitStament = " limit 0," + size + ";";
 		sql += limitStament;
