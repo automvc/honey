c91713a6e69122135ac9b36b5de4c069530cfdeb
Kingstar <honeysoft@126.com>
2019-03-07 15:11:31 +0800

    v1.4.1 adjust log

--- src/main/java/org/honey/osql/core/Logger.java
+++ src/main/java/org/honey/osql/core/Logger.java
@@ -1,5 +1,8 @@
 package org.honey.osql.core;
 
+import org.bee.logging.Log;
+import org.honey.logging.LoggerFactory;
+
 /**
  * @author Kingstar
  * @since  1.0
@@ -8,20 +11,22 @@ public class Logger {
 	
 	private static boolean  showSQL=HoneyConfig.getHoneyConfig().isShowSQL();
 	
+	final static Log log=LoggerFactory.getLogger();
+	
 	public static void print(String s){
-		System.out.println(s);
+		log.info(s);
 	}
 	
 	public static void print(String s1,String s2){
-		System.out.println(s1+" :  "  +s2);
+		log.info(s1+" :  "  +s2);
 	}
 	
 	public static void println(String s1,String s2){
-		System.out.println(s1+"\n"  +s2);
+		log.info(s1+"\n"  +s2);
 	}
 	
 	private static void _println(String s1,String s2){
-		System.out.println(s1+"\n"  +s2);
+		log.info(s1+"\n"  +s2);
 	}
 	
 	public static void logSQL(String hardStr,String sql){
