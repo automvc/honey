e4ac38771feefbbda4f5cdb9feb8bb1fc62ce376
Kingstar <honeysoft@126.com>
2019-03-08 12:05:55 +0800

    v1.4.2 move to org.teasoft

--- src/main/java/org/honey/osql/constant/NullEmpty.java
+++
@@ -1,9 +0,0 @@
-package org.honey.osql.constant;
-
-public interface NullEmpty {
-    int NULL=0;
-    int EMPTY_STRING=1;
-    int NULL_AND_EMPTY_STRING=2;
-    
-    int EXCLUDE=-1;  //null empty两者都排除
-}
