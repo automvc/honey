1a1eb234fed0096ec9064d299c1a26edd3bc6e20
Kingstar <aiteasoft@126.com>
2019-06-02 22:55:38 +0800

    update TransactionExam

--- src/main/java/org/teasoft/honey/osql/example/TransactionExam.java
+++ src/main/java/org/teasoft/honey/osql/example/TransactionExam.java
@@ -5,10 +5,11 @@ import java.util.List;
 
 import org.teasoft.bee.osql.BeeException;
 import org.teasoft.bee.osql.Suid;
+import org.teasoft.bee.osql.transaction.Transaction;
 import org.teasoft.honey.osql.core.BeeFactory;
+import org.teasoft.honey.osql.core.SessionFactory;
 import org.teasoft.honey.osql.example.entity.Orderitem;
 import org.teasoft.honey.osql.example.entity.Orders;
-import org.teasoft.honey.osql.example.entity.User;
 
 /**
  * @author Kingstar
@@ -17,15 +18,16 @@ import org.teasoft.honey.osql.example.entity.User;
 public class TransactionExam {
 
 	public static void main(String[] args) {
+		Transaction transaction=SessionFactory.getTransaction();
 		try {
+			
+			
+			transaction.begin();
+			
 			Suid suid = BeeFactory.getHoneyFactory().getSuid();
 
-			User user = new User();
-			user.setName("testName");
-			user.setEmail("beeUser@163.com");
-			suid.insert(user); //insert
-
 			Orders orders = new Orders();
+			orders.setId(101L);
 			orders.setUserid("bee");
 			orders.setName("Bee-ORM framework");
 			orders.setTotal(new BigDecimal("91.99"));
@@ -34,12 +36,15 @@ public class TransactionExam {
 			suid.insert(orders); //insert
 
 			Orderitem orderitem = new Orderitem();
-			orderitem.setOrderid(100001L);
+			orderitem.setId(12L);
+			orderitem.setOrderid(101L);
 			orderitem.setUserid("bee");
 			orderitem.setCategory("book");
 			orderitem.setPrice(new BigDecimal("30"));
 
 			suid.insert(orderitem);
+			
+			transaction.commit();
 
 			List<Orders> list = suid.select(orders); //可任意组合条件查询
 			for (int i = 0; i < list.size(); i++) {
@@ -48,6 +53,7 @@ public class TransactionExam {
 
 		} catch (BeeException e) {
 			e.printStackTrace();
+			transaction.rollback();
 		}
 	}
 
