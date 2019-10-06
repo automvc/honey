ea661c52e7a4aa5d677918a322a45636a6509c52
Kingstar <honeysoft@126.com>
2019-10-06 20:54:52 +0800

    v1.4.31 Enhance Cache

--- src/main/java/org/teasoft/honey/osql/core/HoneyConfig.java
+++ src/main/java/org/teasoft/honey/osql/core/HoneyConfig.java
@@ -42,7 +42,26 @@ public final class HoneyConfig {
 		setUrl(BeeProp.getBeeProp(DbConfigConst.DB_URL));
 		setUsername(BeeProp.getBeeProp(DbConfigConst.DB_USERNAM));
 		setPassword(BeeProp.getBeeProp(DbConfigConst.DB_PASSWORD));
-
+		
+//		setCacheType(BeeProp.getBeeProp("bee.osql.cache.type"));  //暂时只有FIFO
+		
+		String t1 = BeeProp.getBeeProp("bee.osql.cache.map.size"); //缓存集数据量大小
+		if (t1 != null) setCacheMapSize(Integer.parseInt(t1));
+		
+		String t2 = BeeProp.getBeeProp("bee.osql.cache.timeout");//缓存保存时间(毫秒 ms)
+		if (t2 != null) setCacheTimeout(Integer.parseInt(t2));
+		
+		String t3 = BeeProp.getBeeProp("bee.osql.cache.work.resultSet.size"); //resultset超过一定的值将不会放缓存
+		if (t3 != null) setCacheWorkResultSetSize(Integer.parseInt(t3));
+		
+		String t4 = BeeProp.getBeeProp("bee.osql.cache.startDeleteCache.rate"); 
+		if (t4 != null) setStartDeleteCacheRate(Double.parseDouble(t4));
+		
+		String t5 = BeeProp.getBeeProp("bee.osql.cache.fullUsed.rate"); 
+		if (t5 != null) setCachefullUsedRate(Double.parseDouble(t5));
+		
+		String t6 = BeeProp.getBeeProp("bee.osql.cache.fullClearCache.rate"); 
+		if (t6 != null) setFullClearCacheRate(Double.parseDouble(t6));
 	}
 
 	// 启动时动态获取
@@ -63,6 +82,15 @@ public final class HoneyConfig {
 	private String username;
 	private String password;
 	private int maxResultSize;
+	
+	private int cacheTimeout;
+	private int cacheWorkResultSetSize;
+	private int cacheMapSize;
+	private String cacheType="FIFO";
+	
+	private static double startDeleteCacheRate;  //when timeout use
+	private static double cachefullUsedRate;      //when add element in cache use
+	private static double fullClearCacheRate;  //when add element in cache use
 
 	private void setShowSQL(boolean showSQL) {
 		this.showSQL = showSQL;
@@ -184,4 +212,60 @@ public final class HoneyConfig {
 		this.nullToEmptyStringInReturnStringList = nullToEmptyStringInReturnStringList;
 	}
 
+	public int getCacheTimeout() {
+		return cacheTimeout;
+	}
+
+	public int getCacheMapSize() {
+		return cacheMapSize;
+	}
+
+	private void setCacheTimeout(int cacheTimeout) {
+		this.cacheTimeout = cacheTimeout;
+	}
+
+	private void setCacheMapSize(int cacheMapSize) {
+		this.cacheMapSize = cacheMapSize;
+	}
+
+	public int getCacheWorkResultSetSize() {
+		return cacheWorkResultSetSize;
+	}
+
+	private void setCacheWorkResultSetSize(int cacheWorkResultSetSize) {
+		this.cacheWorkResultSetSize = cacheWorkResultSetSize;
+	}
+
+	public String getCacheType() {
+		return cacheType;
+	}
+
+	private void setCacheType(String cacheType) {
+		this.cacheType = cacheType;
+	}
+
+	private static void setStartDeleteCacheRate(double startDeleteCacheRate) {
+		HoneyConfig.startDeleteCacheRate = startDeleteCacheRate;
+	}
+
+	private static void setCachefullUsedRate(double cachefullUsedRate) {
+		HoneyConfig.cachefullUsedRate = cachefullUsedRate;
+	}
+
+	private static void setFullClearCacheRate(double fullClearCacheRate) {
+		HoneyConfig.fullClearCacheRate = fullClearCacheRate;
+	}
+
+	public static double getStartDeleteCacheRate() {
+		return startDeleteCacheRate;
+	}
+
+	public static double getCachefullUsedRate() {
+		return cachefullUsedRate;
+	}
+
+	public static double getFullClearCacheRate() {
+		return fullClearCacheRate;
+	}
+	
 }
