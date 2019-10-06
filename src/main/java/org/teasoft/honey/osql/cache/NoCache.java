f7eb1beb517b8ad91b6ed01bb77b54b54287d033
Kingstar <honeysoft@126.com>
2019-10-06 20:51:56 +0800

    v1.4.31 Enhance Cache

---
+++ src/main/java/org/teasoft/honey/osql/cache/NoCache.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2013-2019 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.honey.osql.cache;
+
+import org.teasoft.bee.osql.Cache;
+
+/**
+ * @author Kingstar
+ * @since  1.4
+ */
+public class NoCache implements Cache{
+	@Override
+	public Object get(String key) {
+		return null;
+	}
+
+	@Override
+	public void add(String key, Object rs) {
+		//do nothing
+	}
+	
+	@Override
+	public void clear(String tableCacheKey) {
+		//do nothing
+	}
+
+}
