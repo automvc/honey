5536758617ee81fccc30b4101efb09ea6067b70d
Kingstar <honeysoft@126.com>
2019-11-09 23:48:52 +0800

    v1.5.0 1.add NameTranslate interface and default implement class,support customize translation rule of  DB and Java naming.

---
+++ src/main/java/org/teasoft/honey/osql/core/name/UpperCaseUnderScoreAndCamelName.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2016-2019 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.honey.osql.core.name;
+
+/**
+ * @author Kingstar
+ * @since  1.5
+ */
+public class UpperCaseUnderScoreAndCamelName extends UnderScoreAndCamelName{
+
+	@Override
+	public String toTableName(String entityName) {
+		return super.toTableName(entityName).toUpperCase();
+	}
+
+	@Override
+	public String toColumnName(String fieldName) {
+		return super.toColumnName(fieldName).toUpperCase();
+	}
+	
+	
+}
