c5669d9159477053db0df8b1d2523cbee031e943
Kingstar <honeysoft@126.com>
2019-11-09 23:48:21 +0800

    v1.5.0 1.add NameTranslate interface and default implement class,support customize translation rule of  DB and Java naming.

---
+++ src/main/java/org/teasoft/honey/osql/core/name/OriginalName.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2016-2019 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.honey.osql.core.name;
+
+import org.teasoft.bee.osql.NameTranslate;
+
+/**
+ * return original name
+ * 返回原名称
+ * @author Kingstar
+ * @since  1.5
+ */
+public class OriginalName implements NameTranslate{
+
+	@Override
+	public String toTableName(String entityName) {
+		return entityName;
+	}
+
+	@Override
+	public String toColumnName(String fieldName) {
+		return fieldName;
+	}
+
+	@Override
+	public String toEntityName(String tableName) {
+		return tableName;
+	}
+
+	@Override
+	public String toFieldName(String columnName) {
+		return columnName;
+	}
+
+}
