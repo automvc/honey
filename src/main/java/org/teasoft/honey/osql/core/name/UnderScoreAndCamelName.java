f4d5969c2c814aba3e04079f5e7d4d7f357819fa
Kingstar <honeysoft@126.com>
2019-11-09 23:48:36 +0800

    v1.5.0 1.add NameTranslate interface and default implement class,support customize translation rule of  DB and Java naming.

---
+++ src/main/java/org/teasoft/honey/osql/core/name/UnderScoreAndCamelName.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2016-2019 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.honey.osql.core.name;
+
+import org.teasoft.bee.osql.NameTranslate;
+import org.teasoft.honey.osql.core.HoneyConfig;
+
+/**
+ * Java Camel and UnderScore transform.
+ * Java驼峰命名与DB下划线命名互转
+ * @author Kingstar
+ * @since  1.5
+ */
+public class UnderScoreAndCamelName implements NameTranslate{
+	
+	@Override
+	public String toTableName(String entityName) {
+		return NameUtil.toUnderscoreNaming(entityName);
+	}
+
+	@Override
+	public String toColumnName(String fieldName) {
+          return NameUtil.toUnderscoreNaming(fieldName);
+	}
+
+	@Override
+	public String toEntityName(String tableName) {
+		if (HoneyConfig.getHoneyConfig().isDbNamingToLowerCaseBefore()) {
+			//need lowercase first if the name has upper case
+			tableName = tableName.toLowerCase(); 
+		}
+		return NameUtil.firstLetterToUpperCase(NameUtil.toCamelNaming(tableName));
+	}
+
+	@Override
+	public String toFieldName(String columnName) {
+		if (HoneyConfig.getHoneyConfig().isDbNamingToLowerCaseBefore()) {
+			//need lowercase first if the name has upper case
+			columnName = columnName.toLowerCase();  //if not , BEE_NAME->BEENAME  -> ??
+		}
+		return NameUtil.toCamelNaming(columnName);
+	}
+
+}
