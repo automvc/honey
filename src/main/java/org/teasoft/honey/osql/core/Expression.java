a0e29fb8dbcd3d21963b0743786c1372eacbec34
Kingstar <honeysoft@126.com>
2019-11-23 18:57:02 +0800

    v1.6.0 add Expression for select(T entity, Condition condition)

---
+++ src/main/java/org/teasoft/honey/osql/core/Expression.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2016-2019 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.honey.osql.core;
+
+import org.teasoft.bee.osql.Op;
+
+/**
+ * @author Kingstar
+ * @since  1.6
+ */
+public final class Expression {
+
+	String fieldName;
+	String opType;
+	Object value;
+	int opNum;
+	Object value2;
+
+	public Expression() {}
+
+	public Expression(String field, Op opType, Object value) {
+		this.fieldName = field;
+		this.opType = opType.getOperator();
+		this.value = value;
+		this.opNum = 2;
+	}
+
+	public Expression(String field, Object value) {
+		this.fieldName = field;
+		this.opType = Op.eq.getOperator();
+		this.value = value;
+		this.opNum = 2;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public String getOpType() {
+		return opType;
+	}
+
+	public void setOpType(String opType) {
+		this.opType = opType;
+	}
+
+	public Object getValue() {
+		return value;
+	}
+
+	public void setValue(Object value) {
+		this.value = value;
+	}
+
+	public int getOpNum() {
+		return opNum;
+	}
+
+	public void setOpNum(int opNum) {
+		this.opNum = opNum;
+	}
+
+	public Object getValue2() {
+		return value2;
+	}
+
+	public void setValue2(Object value2) {
+		this.value2 = value2;
+	}
+
+}
