f12fc1fd3ea104bbf0c3bfdda6addcb5c79990b0
Kingstar <honeysoft@126.com>
2019-12-02 00:07:43 +0800

    V1.7.0 more tables select support.

---
+++ src/main/java/org/teasoft/honey/osql/core/MoreTableStruct.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2016-2020 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.honey.osql.core;
+
+import java.lang.reflect.Field;
+
+import org.teasoft.bee.osql.annotation.JoinType;
+
+/**
+ * @author Kingstar
+ * @since  1.7
+ */
+class MoreTableStruct {
+	
+	 JoinType joinType;  //联接类型
+	 String mainField; //主表关联的字段
+	 String subField;  //从表关联的字段
+	 String subAlias;  //从表别名
+	//main table don't record above 4 fields.
+	 
+	 boolean hasSubAlias;
+	
+	 String tableName;
+	 String entityName;
+	 String entityFullName; //inclue package
+	 String columnsFull; // eg: tableName.column1,tableName.column2
+	
+	 String joinExpression;  // mainTable.mainField=subtableName.subField  
+                                    //如何有subAlias,用subAlias   mainTable.mainField=subAlias.subField
+	 Field subEntityField;   //for return
+	 Object subObject;
+	 String useSubTableName;  //有别名时,使用别名,而不是表名
+
+}
