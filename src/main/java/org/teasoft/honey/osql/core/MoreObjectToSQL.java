5e03f10daa9af8c5cfd35a9e4c23b3a05ad44d65
Kingstar <honeysoft@126.com>
2019-12-02 00:06:56 +0800

    V1.7.0 more tables select support.

---
+++ src/main/java/org/teasoft/honey/osql/core/MoreObjectToSQL.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2016-2020 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.honey.osql.core;
+
+import org.teasoft.bee.osql.Condition;
+import org.teasoft.bee.osql.MoreObjToSQL;
+
+/**
+ * @author Kingstar
+ * @since  1.7
+ */
+public class MoreObjectToSQL implements MoreObjToSQL{
+
+	@Override
+	public <T> String toSelectSQL(T entity) {
+		return _MoreObjectToSQLHelper._toSelectSQL(entity); // 默认过滤NULL和空字符串
+	}
+
+	@Override
+	public <T> String toSelectSQL(T entity, int start, int size) {
+		return _MoreObjectToSQLHelper._toSelectSQL(entity,start,size); // 默认过滤NULL和空字符串
+	}
+
+	@Override
+	public <T> String toSelectSQL(T entity, Condition condition) {
+		return _MoreObjectToSQLHelper._toSelectSQL(entity); // 若condition没有设置IncludeType,默认过滤NULL和空字符串
+	}
+	
+}
