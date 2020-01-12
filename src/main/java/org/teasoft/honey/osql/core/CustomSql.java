5a46539ca921587b949bc256f45c30dcc303ce96
Kingstar <honeysoft@126.com>
2020-01-12 22:08:00 +0800

    v1.7.1 enhance the exception when the sql string is null.

--- src/main/java/org/teasoft/honey/osql/core/CustomSql.java
+++ src/main/java/org/teasoft/honey/osql/core/CustomSql.java
@@ -1,5 +1,6 @@
 package org.teasoft.honey.osql.core;
 
+import org.teasoft.bee.osql.exception.SqlNullException;
 import org.teasoft.honey.osql.util.PropertiesReader;
 
 /**
@@ -9,15 +10,23 @@ import org.teasoft.honey.osql.util.PropertiesReader;
  * @since  1.0
  */
 public class CustomSql {
-	
+
 	private static PropertiesReader customSqlProp;
-	
+
 	static {
-		customSqlProp=new PropertiesReader("/bee.sql.properties");
+		customSqlProp = new PropertiesReader("/bee.sql.properties");
 	}
-	
-	public static String getCustomSql(String sqlId){
-		return customSqlProp.getValue(sqlId);
+
+	public static String getCustomSql(String sqlId) {
+
+		String sql = customSqlProp.getValue(sqlId);
+		if (sql == null) {
+			throw new SqlNullException("The sql statement string get by sqlId:"+sqlId+", is Null !");
+		} else if ("".equals(sql.trim())) {
+			throw new SqlNullException("The sql statement string get by sqlId:"+sqlId+", is empty !");
+		}
+
+		return sql;
 	}
 
 }
