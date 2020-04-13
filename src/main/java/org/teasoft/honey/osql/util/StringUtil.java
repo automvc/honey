fb602fdd9bd546d89840a3cf03b4ee79e03309ce
Kingstar <honeysoft@126.com>
2020-04-13 18:50:00 +0800

    add replace string With Map

---
+++ src/main/java/org/teasoft/honey/osql/util/StringUtil.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2016-2019 the original author.All rights reserved.
+ * Kingstar(aiteasoft@163.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.honey.osql.util;
+
+import java.util.Map;
+
+import org.teasoft.honey.osql.core.TokenUtil;
+
+/**
+ * @author Kingstar
+ * @since  1.7.2
+ */
+public class StringUtil {
+
+//	public static String replaceWithMap0(String text, Map<String,String> map) {
+//		for (String key : map.keySet()) {
+//			text=text.replace("#{"+key+"}", map.get(key));
+//		}
+//		return text;
+//	}
+	
+	public static String replaceWithMap(String text, Map<String,String> map) {
+
+		return TokenUtil.processWithMap(text, "#{", "}", map);
+
+	}
+
+}
