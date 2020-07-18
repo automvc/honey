88a1a72eb818c9db5d83a6f288c0eafe96fc3d1c
Kingstar <honeysoft@126.com>
2020-07-18 14:35:06 +0800

    提供GenId生成工厂:GenIdFactory

--- src/main/java/org/teasoft/honey/distribution/GenIdFactory.java
+++ src/main/java/org/teasoft/honey/distribution/GenIdFactory.java
@@ -16,24 +16,54 @@ import org.teasoft.bee.distribution.GenId;
  * @since  1.7.3
  */
 public class GenIdFactory {
+
 	private static GenId genId;
-	private static Map<String,GenId> map=new ConcurrentHashMap<>();
+	private static Map<String, GenId> map = new ConcurrentHashMap<>();
+	private static String defaultGenType = "SerialUniqueId";
 	
-	public static long get(String type) {
-		genId=getGenId(type);
+	public static long get() {
+		return get("");
+	}
+	
+	public static long[] getRangeId(int sizeOfIds) {
+		return getRangeId("", sizeOfIds);
+	}
+
+	public static long get(String bizType) {
+		return get(bizType, defaultGenType);
+	}
+
+	public static long get(String bizType, String genType) {
+		genId = getGenId(bizType, genType);
 		return genId.get();
 	}
 
-	public static long[] getRangeId(String type,int sizeOfIds) {
-		genId=getGenId(type);
+	public static long[] getRangeId(String bizType, int sizeOfIds) {
+		return getRangeId(bizType, defaultGenType, sizeOfIds);
+	}
+
+	public static long[] getRangeId(String bizType, String genType, int sizeOfIds) {
+		genId = getGenId(bizType, genType);
 		return genId.getRangeId(sizeOfIds);
 	}
-	
-	private static GenId getGenId(String type){
-		genId=map.get(type);
-		if(genId==null) {
-			genId=new PearFlowerId();  //TODO 要选择不同类型
-			map.put(type, genId);
+
+	private static GenId getGenId(String bizType, String genType) {
+		String key = genType + "::" + bizType;
+		genId = map.get(key);
+		if (genId == null) {
+			switch (genType) {
+
+				case "OneTimeSnowflakeId":
+					genId = new OneTimeSnowflakeId();
+				case "PearFlowerId":
+					genId = new PearFlowerId();
+				case "SerialUniqueId":
+				default:
+					genId = new SerialUniqueId();
+			}
+			map.put(key, genId);
+			//TODO 要选择不同类型   每种ID,还要选择不同的业务类型,如不同的表名,只给自己的表拿ID(表名隔离).
+			//默认用SerialUniqueId, 单机,用workerid=0.  插入到表可以保证单调连续,全局唯一.
 		}
 		return genId;
 	}
