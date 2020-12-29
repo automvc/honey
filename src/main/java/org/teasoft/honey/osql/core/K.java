0c8768bff627293835b6ca15f95b3ea434a272db
Kingstar <honeysoft@126.com>
2020-12-29 11:43:20 +0800

    enhance code quality

--- src/main/java/org/teasoft/honey/osql/core/K.java
+++ src/main/java/org/teasoft/honey/osql/core/K.java
@@ -18,37 +18,37 @@ public class K {
 	
 	private K() {}
 	
-	public static final String space=k.space(),
-		       select=k.select(),
-		       as=k.as(),
-		       from=k.from(), 
-		       where=k.where(),
-		       insert=k.insert(),
-		       into=k.into(),
-		       values=k.values(),
-		       and=k.and(),
-		       or=k.or(),
-		       Null=k.Null(),
-		       isNull=k.isNull(),
-		       isNotNull=k.isNotNull(),
-		       
-		       update=k.update(),
-		       set=k.set(),
-		       delete=k.delete(),
-		       orderBy=k.orderBy(),
-		       count=k.count(),
-		       asc=k.asc(),
-		       on=k.on(),
-		       
-		       limit=k.limit(),
-		       offset=k.offset(),
-		       top=k.top(),
-		       
-		       groupBy=k.groupBy(),
-		       having=k.having(),
-		       between=k.between(),
-		       notBetween=k.notBetween(),
-		       
-		       forUpdate=k.forUpdate();
+	public static final String space=k.space();
+	public static final String select=k.select();
+	public static final String as=k.as();
+	public static final String from=k.from(); 
+	public static final String where=k.where();
+	public static final String insert=k.insert();
+	public static final String into=k.into();
+	public static final String values=k.values();
+	public static final String and=k.and();
+	public static final String or=k.or();
+	public static final String Null=k.Null();
+	public static final String isNull=k.isNull();
+	public static final String isNotNull=k.isNotNull();
+
+	public static final String update=k.update();
+	public static final String set=k.set();
+	public static final String delete=k.delete();
+	public static final String orderBy=k.orderBy();
+	public static final String count=k.count();
+	public static final String asc=k.asc();
+	public static final String on=k.on();
+
+	public static final String limit=k.limit();
+	public static final String offset=k.offset();
+	public static final String top=k.top();
+
+	public static final String groupBy=k.groupBy();
+	public static final String having=k.having();
+	public static final String between=k.between();
+	public static final String notBetween=k.notBetween();
+
+	public static final String forUpdate=k.forUpdate();
 
 }
