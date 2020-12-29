1948cc1f2df60dcfdc9c31f6fbecac1f0855c5b0
Kingstar <honeysoft@126.com>
2020-12-29 11:13:43 +0800

    enhance code quality

--- src/main/java/org/teasoft/honey/osql/constant/DbConfigConst.java
+++ src/main/java/org/teasoft/honey/osql/constant/DbConfigConst.java
@@ -5,6 +5,6 @@ public interface DbConfigConst {
 	String DB_DRIVERNAME="bee.db.driverName";
 	String DB_URL="bee.db.url";
 	String DB_USERNAM="bee.db.username";
-	String DB_PASSWORD="bee.db.password";
+	String DB_PWORD="bee.db.password";
 			
 }
