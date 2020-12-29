541721d709d88c32152490f113c03d00ef5a1829
Kingstar <honeysoft@126.com>
2020-12-29 11:23:23 +0800

    enhance code quality

--- src/main/java/org/teasoft/honey/osql/util/DateUtil.java
+++ src/main/java/org/teasoft/honey/osql/util/DateUtil.java
@@ -19,10 +19,11 @@ import org.teasoft.honey.osql.core.Logger;
 public class DateUtil {
 	
 	
-	private static SimpleDateFormat defaultFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	private static SimpleDateFormat format =null;
+//	private static SimpleDateFormat defaultFormat =null;  // new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//	private static SimpleDateFormat format =null;
 	
-	static { //since v1.7.2
+	private static SimpleDateFormat getSimpleDateFormat() {
+		SimpleDateFormat defaultFormat =null;  
 		String dateFormatStr = HoneyConfig.getHoneyConfig().getDateFormat();
 		if (dateFormatStr != null && !"".equals(dateFormatStr.trim())) {
 			try {
@@ -31,16 +32,22 @@ public class DateUtil {
 				Logger.warn("In DateUtil: it is error date format String :"+dateFormatStr);
 				defaultFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 			}
+		}else {
+			defaultFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		}
+		
+		
+		return defaultFormat;
 	}
 	
 	public static String currentDate(){
-		return defaultFormat.format(new Date());
+		return getSimpleDateFormat().format(new Date());
 	}
 	
 	public static String currentDate(String formatStr){
+		SimpleDateFormat format =null;  
 		if(formatStr==null || "".trim().equals(formatStr)) 
-			format=defaultFormat;
+			format=getSimpleDateFormat();
 		else 
 		   format = new SimpleDateFormat(formatStr);
 		return format.format(new Date());
