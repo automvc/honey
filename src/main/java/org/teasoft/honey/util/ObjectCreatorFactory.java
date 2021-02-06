22d1fda60bee3d5809066b32a68844575084b92b
Kingstar <honeysoft@126.com>
2021-02-07 00:28:25 +0800

    ObjectCreator

---
+++ src/main/java/org/teasoft/honey/util/ObjectCreatorFactory.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2016-2021 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.honey.util;
+
+/**
+ * @author Kingstar
+ * @since  1.9
+ */
+public class ObjectCreatorFactory {
+	public static Object create(String s, Class c) {
+
+		if (c == null) return null;
+
+		if (c.equals(String.class)) {
+			return ObjectCreator.createString(s);
+		} else if (c.equals(int.class) || c.equals(Integer.class)) {
+			return ObjectCreator.createInt(s);
+		} else if (c.equals(short.class) || c.equals(Short.class)) {
+			return ObjectCreator.createShort(s);
+		} else if (c.equals(byte.class) || c.equals(Byte.class)) {
+			return ObjectCreator.createByte(s);
+		} else if (c.equals(double.class) || c.equals(Double.class)) {
+			return ObjectCreator.createDouble(s);
+		} else if (c.equals(long.class) || c.equals(Long.class)) {
+			return ObjectCreator.createLong(s);
+		} else if (c.equals(boolean.class) || c.equals(Boolean.class)) {
+			return ObjectCreator.createBoolean(s);
+		} else {
+			return null;
+//			throw new Exception("Not support the type: " + c.getName());
+		}
+	}
+}
