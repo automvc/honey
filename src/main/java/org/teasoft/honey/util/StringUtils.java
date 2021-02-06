fcf168eac0d2f5d5fec4f3156ca41a98601c2503
Kingstar <honeysoft@126.com>
2021-02-07 00:29:08 +0800

    util

--- src/main/java/org/teasoft/honey/util/StringUtils.java
+++ src/main/java/org/teasoft/honey/util/StringUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2020 the original author.All rights reserved.
+ * Copyright 2016-2021 the original author.All rights reserved.
  * Kingstar(honeysoft@126.com)
  * The license,see the LICENSE file.
  */
@@ -27,4 +27,13 @@ public class StringUtils {
     public static boolean isNotEmpty(final String str) {
         return !isEmpty(str);
     }
+    
+    
+	public static boolean isEmpty(final String strings[]) {
+		return strings == null || strings.length == 0;
+	}
+	
+	public static boolean isNotEmpty(final String strings[]) {
+		return !isEmpty(strings);
+	}
 }
