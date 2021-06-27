f060fd9f8674a4eea82137fa0ade67c016b46640
Kingstar <honeysoft@126.com>
2021-06-27 23:24:42 +0800

    add NameCheckUtil

---
+++ src/main/java/org/teasoft/honey/osql/util/NameCheckUtil.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2016-2021 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.honey.osql.util;
+
+import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import org.teasoft.bee.osql.LowerKey;
+import org.teasoft.bee.osql.exception.BeeErrorNameException;
+import org.teasoft.honey.osql.core.Logger;
+
+/**
+ * @author Kingstar
+ * @since  1.9
+ */
+public class NameCheckUtil {
+	private static Set<String> keywordSet;
+	static {
+		keywordSet = _keywordSet();
+	}
+
+	public static Set<String> keywordSet() {
+		return keywordSet;
+	}
+
+	public static boolean isKeyName(String name) {
+		return keywordSet.contains(name);
+	}
+
+	private static Set<String> _keywordSet() {
+		LowerKey entity = new LowerKey();
+		Field fields[] = entity.getClass().getDeclaredFields();
+		int len = fields.length;
+		Set<String> set = new HashSet<>();
+		try {
+			for (int i = 0; i < len; i++) {
+				fields[i].setAccessible(true);
+				set.add(fields[i].get(entity).toString());
+			}
+		} catch (Exception e) {
+		}
+
+		return set;
+	}
+
+	public static boolean isValidName(String name) {
+		String p = "^[a-zA-Z]{1}[0-9a-zA-Z_]{0,}$";
+		Pattern pattern = Pattern.compile(p);
+		return pattern.matcher(name).find();
+	}
+
+	public static boolean isNotValidName(String name) {
+		return !isValidName(name);
+	}
+
+	public static void checkName(String name) {
+
+		if (NameCheckUtil.isKeyName(name)) {
+			Logger.warn("The name : " + name + " , it is key word name!");
+		}
+
+		if (NameCheckUtil.isValidName(name)) {
+			return;
+		} else {
+			if (isIllegal(name)) {
+				throw new BeeErrorNameException("The name: '" + name + "' is illegal!");
+			} else {
+				Logger.warn("The name is " + name + " , does not conform to naming conventions!");
+			}
+		}
+
+	}
+
+	public static boolean isIllegal(String fieldName) {
+		if (fieldName == null || fieldName.contains(" ") || fieldName.contains("-")
+				|| fieldName.contains("#") || fieldName.contains("|") || fieldName.contains("+")
+				|| fieldName.contains(";")) {
+			return true;
+		}
+		return false;
+	}
+
+}
