69d5fb7247b7099deee885604ed232a64bc53d0f
Kingstar <honeysoft@126.com>
2022-01-01 10:55:39 +0800

    BF is shortcut for BeeFactoryHelper

---
+++ src/main/java/org/teasoft/honey/osql/shortcut/BF.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2016-2021 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.honey.osql.shortcut;
+
+import org.teasoft.honey.osql.core.BeeFactoryHelper;
+
+/**
+ * shortcut for BeeFactoryHelper
+ * @author Kingstar
+ * @since  1.9.8
+ */
+public class BF extends BeeFactoryHelper{
+
+}
