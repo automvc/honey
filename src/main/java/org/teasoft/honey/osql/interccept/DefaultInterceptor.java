226a5c66b35b3c15cefd1a761dd15c45855b193c
Kingstar <honeysoft@126.com>
2022-02-11 19:47:29 +0800

    update DatetimeHandler

--- src/main/java/org/teasoft/honey/osql/interccept/DefaultInterceptor.java
+++ src/main/java/org/teasoft/honey/osql/interccept/DefaultInterceptor.java
@@ -6,9 +6,14 @@
 
 package org.teasoft.honey.osql.interccept;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
+import org.teasoft.bee.osql.SuidType;
 import org.teasoft.bee.osql.interccept.Interceptor;
+import org.teasoft.honey.osql.core.HoneyContext;
+import org.teasoft.honey.osql.interccept.annotation.DatetimeHandler;
+import org.teasoft.honey.osql.util.AnnoUtil;
 
 /**
  * @author Kingstar
@@ -19,7 +24,30 @@ public class DefaultInterceptor implements Interceptor{
 	private String ds;
 	
 	@Override
-	public Object beforePasreEntity(Object entity) {
+	public Object beforePasreEntity(Object entity,SuidType suidType) {
+		Boolean f=HoneyContext.getEntityInterceptorFlag(entity.getClass().getName());
+		if(f==Boolean.FALSE) return entity;
+		
+		Field fields[] = entity.getClass().getDeclaredFields(); 
+		int len = fields.length;
+		boolean isHas=false;
+		for (int i = 0; i < len; i++) {
+			fields[i].setAccessible(true);
+			if (AnnoUtil.isDatetime(fields[i])) {
+				if(f==null && !isHas) isHas=true;
+				DatetimeHandler.process(fields[i], entity,suidType);
+			}else if(AnnoUtil.isCreatetime(fields[i])) {
+				if(f==null && !isHas) isHas=true;
+				DatetimeHandler.processCreatetime(fields[i], entity, suidType);
+			}else if(AnnoUtil.isUpdatetime(fields[i])) {
+				if(f==null && !isHas) isHas=true;
+				DatetimeHandler.processUpdatetime(fields[i], entity, suidType);
+			}
+		}
+		
+		if(f==null)
+		  HoneyContext.addEntityInterceptorFlag(entity.getClass().getName(), isHas);
+		
 		return entity;
 	}
 	
@@ -40,12 +68,12 @@ public class DefaultInterceptor implements Interceptor{
 
 	@Override
 	@SuppressWarnings("rawtypes")
-	public void afterAccessDB(List list) {
+	public void beforeReturn(List list) {
 		
 	}
 	
 	@Override
-	public void afterAccessDB() {
+	public void beforeReturn() {
 	}
 
 }
