9134d1071ae7ee313013bbeacbec6f3e2d054bc5
Kingstar <honeysoft@126.com>
2022-02-14 09:44:20 +0800

    add util

---
+++ src/main/java/org/teasoft/honey/util/EntityUtil.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2016-2022 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.honey.util;
+
+import java.lang.reflect.Field;
+
+import org.teasoft.honey.osql.core.HoneyUtil;
+import org.teasoft.honey.osql.core.NameTranslateHandle;
+
+/**
+ * 实体相关工具类
+ * @author Kingstar
+ * @since  1.11
+ */
+public class EntityUtil {
+
+	/**
+	 * 获取实体的字段名称,不作命名转换
+	 * 会忽略部分注解字段
+	 * @param entity 实体对象
+	 * @return 实体的字段名称
+	 */
+	public static String getFieldNames(Object entity) {
+		return getFieldNames(entity, false);
+	}
+
+	/**
+	 * 获取实体的字段名称
+	 * 会忽略部分注解字段
+	 * @param entity 实体对象
+	 * @param isTransform 是否命名转换标识
+	 * @return 实体的字段名称
+	 */
+	public static String getFieldNames(Object entity, boolean isTransform) {
+		if (entity == null) return "";
+		Field fields[] = entity.getClass().getDeclaredFields();
+
+		if (fields == null) return "";
+
+		StringBuffer s = new StringBuffer();
+		int len = fields.length;
+		boolean isFirst = true;
+
+		for (int i = 0; i < len; i++) {
+			if (HoneyUtil.isSkipField(fields[i])) continue;
+			if (HoneyUtil.isSkipFieldJustFetch(fields[i])) continue;
+
+			if (isFirst) {
+				isFirst = false;
+			} else {
+				s.append(",");
+			}
+			if (isTransform)
+				s.append(NameTranslateHandle.toColumnName(fields[i].getName()));
+			else
+				s.append(fields[i].getName());
+
+		}
+		return s.toString();
+
+	}
+
+}
---
+++ src/main/java/org/teasoft/honey/util/SqlKeyCheck.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2016-2022 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.honey.util;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.teasoft.bee.osql.LowerKey;
+
+/**
+ * @author Kingstar
+ * @since  1.11
+ */
+public class SqlKeyCheck {
+
+	private static final String VALUE = "1";
+	//	private static Map<String,String> keyMap=new HashMap<>();
+	private static Map<String, String> keyMap = new TreeMap<>();
+	private static final String keyStr = "table,column,key," 
+	        + "Explain,comment,"
+			+ "group,By,order,Null,is,for," 
+			+ "inner,left,right,join";
+
+	static {
+		initKeyMap(keyStr);
+		String fs = EntityUtil.getFieldNames(new LowerKey());
+		initKeyMap2(fs);
+	}
+
+	private static void initKeyMap(String keyStr) {
+		String keys[] = keyStr.split(",");
+		for (int i = 0; i < keys.length; i++) {
+			keyMap.put(keys[i].toLowerCase(), VALUE);
+		}
+	}
+
+	//不包含大写字母才是独立关键字
+	private static void initKeyMap2(String keyStr) {
+		String keys[] = keyStr.split(",");
+		for (int i = 0; i < keys.length; i++) {
+			if (!StringUtils.isContainUpperCase(keys[i])) 
+				keyMap.put(keys[i].toLowerCase(), VALUE);
+		}
+	}
+
+	public static boolean isKeyWord(String word) {
+		if (keyMap.containsKey(word.toLowerCase()))
+			return true;
+		else
+			return false;
+	}
+
+}
