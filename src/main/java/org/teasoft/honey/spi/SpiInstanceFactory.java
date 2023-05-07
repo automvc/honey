b34627b547a24dc31586cc3d4ab3a8915c11f3bc
Kingstar <honeysoft@126.com>
2023-05-07 11:42:27 +0800

    support fastjson

--- src/main/java/org/teasoft/honey/spi/SpiInstanceFactory.java
+++ src/main/java/org/teasoft/honey/spi/SpiInstanceFactory.java
@@ -11,16 +11,34 @@ import org.teasoft.honey.osql.core.Logger;
 
 /**
  * @author AiTeaSoft
- * @since  2.0
+ * @since 2.0
  */
 public class SpiInstanceFactory {
 
+//	@SuppressWarnings("deprecation")
 	public static JsonTransform getJsonTransform() {
 
 		JsonTransform jsonTransform = SpiInstanceRegister.getInstance(JsonTransform.class);
 		try {
-			if (jsonTransform == null) jsonTransform = (JsonTransform) Class
-					.forName("org.teasoft.beex.spi.JsonTransformDefault").newInstance();
+//			if (jsonTransform == null) jsonTransform = (JsonTransform) Class
+//					.forName("org.teasoft.beex.spi.JsonTransformDefault").newInstance();
+
+//			if (jsonTransform == null) jsonTransform = (JsonTransform) Class
+//					.forName("org.teasoft.beex.spi.FastJsonTransform").newInstance();
+
+			try {
+				// 1.jackson
+				if (jsonTransform == null)
+					jsonTransform = (JsonTransform) Class.forName("org.teasoft.beex.spi.JsonTransformDefault")
+							.getDeclaredConstructor().newInstance();
+			} catch (Exception e) {
+				Logger.debug(e.getMessage(), e);
+			}
+			// 2.fastjson
+			if (jsonTransform == null)
+				jsonTransform = (JsonTransform) Class.forName("org.teasoft.beex.spi.FastJsonTransform")
+						.getDeclaredConstructor().newInstance();
+
 		} catch (Exception e) {
 			Logger.error(e.getMessage(), e);
 		}
