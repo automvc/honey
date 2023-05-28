11349fbab93acaa7e3c80047750ad789c99523a4
Kingstar <honeysoft@126.com>
2023-05-29 00:00:01 +0800

    V1.17.21 sync from V2.1 :完善:Json String类型查询结果转换增强(#84 #I78XVZ)

--- src/main/java/org/teasoft/honey/osql/core/TransformResultSet.java
+++ src/main/java/org/teasoft/honey/osql/core/TransformResultSet.java
@@ -6,6 +6,7 @@
 
 package org.teasoft.honey.osql.core;
 
+import java.lang.reflect.Field;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -13,6 +14,8 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.teasoft.honey.osql.util.AnnoUtil;
 
 /**
  * Transform ResultSet.
@@ -35,35 +38,60 @@ public class TransformResultSet {
 		boolean timeWithMillisecond = HoneyConfig.getHoneyConfig().selectJson_timeWithMillisecond;
 		boolean timestampWithMillisecond = HoneyConfig.getHoneyConfig().selectJson_timestampWithMillisecond;
 		boolean longToString = HoneyConfig.getHoneyConfig().selectJson_longToString;
-
+//        int rowCount=0;
+        boolean isJsonString=false;
+        Field currField=null;
+        String fieldName="";
+        String fieldTypeName="";
+        
 		while (rs.next()) {
+//			rowCount++;
 			json.append(",{");
 			for (int i = 1; i <= columnCount; i++) { //1..n
 				if (rs.getString(i) == null && ignoreNull) {
 					continue;
 				}
+				
+				isJsonString=false;
+				fieldName=_toFieldName(rmeta.getColumnName(i),entityClass);
+				fieldTypeName=HoneyUtil.getFieldType(rmeta.getColumnTypeName(i));
+				
 				json.append("\"");
-//				json.append(rmeta.getColumnName(i));
-				json.append(_toFieldName(rmeta.getColumnName(i),entityClass));
+				json.append(fieldName);
 				json.append("\":");
 
 				if (rs.getString(i) != null) {
-
-					if ("String".equals(HoneyUtil.getFieldType(rmeta.getColumnTypeName(i)))) { // equals改为不区分大小写  其它几个也是.  不需要,Map中值是这种命名风格的
+					
+					temp=rs.getString(i);
+					
+					//Json类型,不用再转换引号
+					if ("JSON".equals(fieldTypeName) ) {
+						isJsonString=true;
+					}else if(entityClass!=null){
+						try {
+							currField = entityClass.getDeclaredField(fieldName);
+							isJsonString=isJoson(currField);
+						} catch (NoSuchFieldException e) {
+							//ignore
+						}
+					}
+					
+					if(isJsonString) {
+						json.append(temp);
+					}else if ("String".equals(fieldTypeName)) { // equals改为不区分大小写  其它几个也是.  不需要,Map中值是这种命名风格的
 						json.append("\"");
-						//json.append(rs.getString(i));
-						temp=rs.getString(i);
+						
 						temp=temp.replace("\\", "\\\\"); //1
 						temp=temp.replace("\"", "\\\""); //2
 						
 						json.append(temp);
 						json.append("\"");
-					} else if ("Date".equals(HoneyUtil.getFieldType(rmeta.getColumnTypeName(i)))) {
+					} else if ("Date".equals(fieldTypeName)) {
 						if (dateWithMillisecond) {
 							json.append(rs.getDate(i).getTime());
 						} else {
 							try {
-								temp = rs.getString(i);
+//								temp = rs.getString(i);
 								Long.valueOf(temp); //test value
 								json.append(temp);
 							} catch (NumberFormatException e) {
@@ -72,12 +100,12 @@ public class TransformResultSet {
 								json.append("\"");
 							}
 						}
-					} else if ("Time".equals(HoneyUtil.getFieldType(rmeta.getColumnTypeName(i)))) {
+					} else if ("Time".equals(fieldTypeName)) {
 						if (timeWithMillisecond) {
 							json.append(rs.getTime(i).getTime());
 						} else {
 							try {
-								temp = rs.getString(i);
+//								temp = rs.getString(i);
 								Long.valueOf(temp); //test value
 								json.append(temp);
 							} catch (NumberFormatException e) {
@@ -86,12 +114,12 @@ public class TransformResultSet {
 								json.append("\"");
 							}
 						}
-					} else if ("Timestamp".equals(HoneyUtil.getFieldType(rmeta.getColumnTypeName(i)))) {
+					} else if ("Timestamp".equals(fieldTypeName)) {
 						if (timestampWithMillisecond) {
 							json.append(rs.getTimestamp(i).getTime());
 						} else {
 							try {
-								temp = rs.getString(i);
+//								temp = rs.getString(i);
 								Long.valueOf(temp); //test value
 								json.append(temp);
 							} catch (NumberFormatException e) {
@@ -100,7 +128,7 @@ public class TransformResultSet {
 								json.append("\"");
 							}
 						}
-					} else if (longToString && "Long".equals(HoneyUtil.getFieldType(rmeta.getColumnTypeName(i)))) {
+					} else if (longToString && "Long".equals(fieldTypeName)) {
 						json.append("\"");
 						json.append(rs.getString(i));
 						json.append("\"");
@@ -112,7 +140,7 @@ public class TransformResultSet {
 					json.append(rs.getString(i));
 				}
 
-				if (i != columnCount) json.append(",");  //bug,  if last field is null and ignore.
+				if (i != columnCount) json.append(",");  //fixed bug.  if last field is null and ignore.
 			} //one record end
 			if(json.toString().endsWith(",")) json.deleteCharAt(json.length()-1); //fix bug
 			json.append("}");
@@ -122,13 +150,22 @@ public class TransformResultSet {
 		}
 		json.insert(0, "[");
 		json.append("]");
-
+		
+//		JsonResultWrap wrap =new JsonResultWrap();
+//		wrap.setResultJson(json.toString());
+//		wrap.setRowCount(rowCount);
+		
 		return json;
 	}
 	
 	@SuppressWarnings("rawtypes")
 	private static String _toFieldName(String columnName,Class entityClass) {
 		return NameTranslateHandle.toFieldName(columnName,entityClass);
+	}
+	
+	//检测是否有Json注解
+	private static boolean isJoson(Field field) {
+		return AnnoUtil.isJson(field);
 	}
 
 	public static List<String[]> toStringsList(ResultSet rs) throws SQLException {
