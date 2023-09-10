6db6c58cee57caf15b94912b5948e5ad0bf7ff2b
Kingstar <honeysoft@126.com>
2023-09-10 23:06:23 +0800

    BootApplicationProp for use spring boot in main class

---
+++ src/main/java/org/teasoft/honey/osql/core/BootApplicationProp.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2016-2023 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.honey.osql.core;
+
+import java.util.Set;
+
+import org.teasoft.bee.osql.Properties;
+import org.teasoft.honey.osql.util.PropertiesReader;
+
+/**
+ * @author Kingstar
+ * @since  2.1.8
+ */
+public class BootApplicationProp implements Properties{
+	
+	static final String SPRING_DOT = "spring.";
+	static final String DATASOURCE_DRIVER_CLASS_NAME2 = SPRING_DOT+ "datasource.driver-class-name";
+	static final String DATASOURCE_DRIVER_CLASS_NAME = SPRING_DOT+ "datasource.driverClassName";
+	static final String DATASOURCE_PASSWORD = SPRING_DOT + "datasource.password";
+	static final String DATASOURCE_USERNAME = SPRING_DOT + "datasource.username";
+	static final String DATASOURCE_URL = SPRING_DOT + "datasource.url";
+
+	private PropertiesReader bootPropReader;
+	
+	public BootApplicationProp() {
+		bootPropReader = new PropertiesReader("application.properties");
+	}
+	
+	public String getProp(String key) {
+		return bootPropReader.getValue(key);
+	}
+
+	public  String getPropText(String key) {
+		return bootPropReader.getValueText(key);
+	}
+	
+	public Set<String> getKeys() {
+		return bootPropReader.getKeys();
+	}
+}
