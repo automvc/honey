c6a6bc769c32ba56b7f9d4de51b55f6ca19f497f
Kingstar <honeysoft@126.com>
2024-12-21 23:06:34 +0800

    for V2.5.0

--- src/main/java/org/teasoft/honey/util/HoneyVersion.java
+++ src/main/java/org/teasoft/honey/util/HoneyVersion.java
@@ -14,7 +14,7 @@ public final class HoneyVersion {
 	
 	private HoneyVersion() {}
 
-	public static final String version = "2.4.2";
-	public static final String buildId = version + ".12";
+	public static final String version = "2.5.0";
+	public static final String buildId = version + ".1";
 
 }
