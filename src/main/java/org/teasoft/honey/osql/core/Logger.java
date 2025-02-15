622ff2dca159d72a5f9aec557e785c2c87efc1f3
Kingstar <honeysoft@126.com>
2025-02-16 04:41:13 +0800

    just format

--- src/main/java/org/teasoft/honey/osql/core/Logger.java
+++ src/main/java/org/teasoft/honey/osql/core/Logger.java
@@ -13,41 +13,40 @@ import org.teasoft.honey.logging.LoggerFactory;
 public class Logger {
 
 	private Logger() {}
-	
+
 	private static boolean isShowSQL() {
 		return HoneyConfig.getHoneyConfig().showSQL;
 	}
-	
+
 	private static boolean isShowSQLShowType() {
 		return HoneyConfig.getHoneyConfig().showSql_showType;
 	}
-	
+
 	private static boolean isShowExecutableSql() {
 		return HoneyConfig.getHoneyConfig().showSql_showExecutableSql;
 	}
-	
+
 	private static String getSqlLoggerLevel() {
 		return HoneyConfig.getHoneyConfig().sqlLoggerLevel;
 	}
-	
 
 	private static Log log = null;
 
-	//专门用于Bee框架输出SQL日志.
+	// 专门用于Bee框架输出SQL日志.
 	static void logSQL(String hardStr) {
 		logSQL(hardStr, "");
 	}
-	 
-	//专门用于Bee框架输出SQL日志.
-	 static void logSQL(String hardStr, String sql) {
+
+	// 专门用于Bee框架输出SQL日志.
+	static void logSQL(String hardStr, String sql) {
 
 		if (!isShowSQL()) return;
-		
-		//=================================start
+
+		// =================================start
 		if (LoggerFactory.isNoArgInConstructor()) {
 			resetLog();
 		} else {
-			//不能移走,它表示的是调用所在的位置.
+			// 不能移走,它表示的是调用所在的位置.
 			String callerClass = "";
 			try {
 				callerClass = sun.reflect.Reflection.getCallerClass().getName();
@@ -64,22 +63,22 @@ public class Logger {
 			}
 			resetLog(callerClass);
 		}
-		//=================================end
+		// =================================end
 
 		if (isShowSQL()) {
 			List list = null;
 			String insertIndex = (String) OneTimeParameter.getAttribute("_SYS_Bee_BatchInsert");
 			if (HoneyUtil.isMysql() && insertIndex != null) {
-				//				mysql批处理,在v1.8开始,不会用于占位设值. 需要清除
+				// mysql批处理,在v1.8开始,不会用于占位设值. 需要清除
 				list = HoneyContext.getAndClearPreparedValue(sql);
 			} else {
-				list = HoneyContext.justGetPreparedValue(sql); //enhance  数字也来到这   可能是影响行数?
+				list = HoneyContext.justGetPreparedValue(sql); // enhance 数字也来到这 可能是影响行数?
 			}
 //			list = HoneyContext._justGetPreparedValue(sql); //统一用这个.  bug,只用于打印的,没有删
 
 			String value = HoneyUtil.list2Value(list, isShowSQLShowType());
-			
-			sql = HoneyUtil.deletePrefix(sql); //2.2
+
+			sql = HoneyUtil.deletePrefix(sql); // 2.2
 
 //			if (value == null || "".equals(value.trim())) {
 			if (value == null) {
@@ -100,9 +99,10 @@ public class Logger {
 
 				if (isShowExecutableSql()) {
 					String executableSql = HoneyUtil.getExecutableSql(sql, list);
-					if (insertIndex != null ) {
+					if (insertIndex != null) {
 						int endIndex = executableSql.indexOf("]_End ");
-						_println("[Bee] " + hardStr + " ( ExecutableSql " + executableSql.substring(4, endIndex + 1) + " )", HoneyUtil.sqlFormat(executableSql.substring(endIndex + 6)) );
+						_println("[Bee] " + hardStr + " ( ExecutableSql " + executableSql.substring(4, endIndex + 1)
+								+ " )", HoneyUtil.sqlFormat(executableSql.substring(endIndex + 6)));
 					} else {
 						_println("[Bee] " + hardStr + " ( ExecutableSql )", HoneyUtil.sqlFormat(executableSql));
 					}
@@ -110,15 +110,15 @@ public class Logger {
 			}
 		}
 	}
-	 
-	//专门用于Bee框架输出SQL日志.
+
+	// 专门用于Bee框架输出SQL日志.
 	private static void print(String s) {
-		//在此判断输出日志的级别. 
-		//用户可以自己定义输出sql的日志级别. 比如定义warn才输出sql.
-		//没意义.  因有一个是否显示sql日志了?   但,如果log4j设置了warn, 它还会输出吗? (用log4j时)    不会输出了,所以还是要设置.
+		// 在此判断输出日志的级别.
+		// 用户可以自己定义输出sql的日志级别. 比如定义warn才输出sql.
+		// 没意义. 因有一个是否显示sql日志了? 但,如果log4j设置了warn, 它还会输出吗? (用log4j时) 不会输出了,所以还是要设置.
 //		log.info(s);
-		
-		//v1.9.8
+
+		// v1.9.8
 		if (getSqlLoggerLevel() == null)
 			log.info(s);
 		else if ("warn".equalsIgnoreCase(getSqlLoggerLevel()))
@@ -129,10 +129,10 @@ public class Logger {
 			log.info(s);
 	}
 
-	//专门用于Bee框架输出SQL日志.
+	// 专门用于Bee框架输出SQL日志.
 	private static void _print(String s1, String s2) {
-		//		log.info(s1+"\n"  +s2);
-		//v1.9.8
+		// log.info(s1+"\n" +s2);
+		// v1.9.8
 		if (getSqlLoggerLevel() == null)
 			log.info(s1 + s2);
 		else if ("warn".equalsIgnoreCase(getSqlLoggerLevel()))
@@ -143,9 +143,9 @@ public class Logger {
 			log.info(s1 + s2);
 	}
 
-	//专门用于Bee框架输出SQL日志.
+	// 专门用于Bee框架输出SQL日志.
 	private static void _println(String s1, String s2) {
-		//v1.9.8
+		// v1.9.8
 		if (getSqlLoggerLevel() == null)
 			log.info(s1 + "\n" + s2);
 		else if ("warn".equalsIgnoreCase(getSqlLoggerLevel()))
@@ -158,11 +158,11 @@ public class Logger {
 
 	public static void debug(String msg) {
 
-		//=================================start
+		// =================================start
 		if (LoggerFactory.isNoArgInConstructor()) {
 			resetLog();
 		} else {
-			//不能移走,它表示的是调用所在的位置.
+			// 不能移走,它表示的是调用所在的位置.
 			String callerClass = "";
 			try {
 				callerClass = sun.reflect.Reflection.getCallerClass().getName();
@@ -179,18 +179,18 @@ public class Logger {
 			}
 			resetLog(callerClass);
 		}
-		//=================================end
+		// =================================end
 
 		log.debug(msg);
 	}
-	
-	public static void debug(String msg,Throwable t) {
 
-		//=================================start
+	public static void debug(String msg, Throwable t) {
+
+		// =================================start
 		if (LoggerFactory.isNoArgInConstructor()) {
 			resetLog();
 		} else {
-			//不能移走,它表示的是调用所在的位置.
+			// 不能移走,它表示的是调用所在的位置.
 			String callerClass = "";
 			try {
 				callerClass = sun.reflect.Reflection.getCallerClass().getName();
@@ -207,18 +207,18 @@ public class Logger {
 			}
 			resetLog(callerClass);
 		}
-		//=================================end
+		// =================================end
 
-		log.debug(msg,t);
+		log.debug(msg, t);
 	}
 
 	public static void info(String msg) {
 
-		//=================================start
+		// =================================start
 		if (LoggerFactory.isNoArgInConstructor()) {
 			resetLog();
 		} else {
-			//不能移走,它表示的是调用所在的位置.
+			// 不能移走,它表示的是调用所在的位置.
 			String callerClass = "";
 			try {
 				callerClass = sun.reflect.Reflection.getCallerClass().getName();
@@ -235,18 +235,18 @@ public class Logger {
 			}
 			resetLog(callerClass);
 		}
-		//=================================end
+		// =================================end
 
 		log.info(msg);
 	}
 
 	public static void info(Number msg) {
 
-		//=================================start
+		// =================================start
 		if (LoggerFactory.isNoArgInConstructor()) {
 			resetLog();
 		} else {
-			//不能移走,它表示的是调用所在的位置.
+			// 不能移走,它表示的是调用所在的位置.
 			String callerClass = "";
 			try {
 				callerClass = sun.reflect.Reflection.getCallerClass().getName();
@@ -263,18 +263,18 @@ public class Logger {
 			}
 			resetLog(callerClass);
 		}
-		//=================================end
+		// =================================end
 
 		log.info(msg + "");
 	}
 
 	public static void warn(String msg) {
 
-		//=================================start
+		// =================================start
 		if (LoggerFactory.isNoArgInConstructor()) {
 			resetLog();
 		} else {
-			//不能移走,它表示的是调用所在的位置.
+			// 不能移走,它表示的是调用所在的位置.
 			String callerClass = "";
 			try {
 				callerClass = sun.reflect.Reflection.getCallerClass().getName();
@@ -291,18 +291,18 @@ public class Logger {
 			}
 			resetLog(callerClass);
 		}
-		//=================================end
+		// =================================end
 
 		log.warn(msg);
 	}
-	
-	public static void warn(String msg,Throwable t) {
 
-		//=================================start
+	public static void warn(String msg, Throwable t) {
+
+		// =================================start
 		if (LoggerFactory.isNoArgInConstructor()) {
 			resetLog();
 		} else {
-			//不能移走,它表示的是调用所在的位置.
+			// 不能移走,它表示的是调用所在的位置.
 			String callerClass = "";
 			try {
 				callerClass = sun.reflect.Reflection.getCallerClass().getName();
@@ -319,18 +319,18 @@ public class Logger {
 			}
 			resetLog(callerClass);
 		}
-		//=================================end
+		// =================================end
 
-		log.warn(msg,t);
+		log.warn(msg, t);
 	}
 
 	public static void warn(Number msg) {
 
-		//=================================start
+		// =================================start
 		if (LoggerFactory.isNoArgInConstructor()) {
 			resetLog();
 		} else {
-			//不能移走,它表示的是调用所在的位置.
+			// 不能移走,它表示的是调用所在的位置.
 			String callerClass = "";
 			try {
 				callerClass = sun.reflect.Reflection.getCallerClass().getName();
@@ -347,18 +347,18 @@ public class Logger {
 			}
 			resetLog(callerClass);
 		}
-		//=================================end
+		// =================================end
 
 		log.warn(msg + "");
 	}
 
 	public static void error(String msg) {
 
-		//=================================start
+		// =================================start
 		if (LoggerFactory.isNoArgInConstructor()) {
 			resetLog();
 		} else {
-			//不能移走,它表示的是调用所在的位置.
+			// 不能移走,它表示的是调用所在的位置.
 			String callerClass = "";
 			try {
 				callerClass = sun.reflect.Reflection.getCallerClass().getName();
@@ -375,18 +375,18 @@ public class Logger {
 			}
 			resetLog(callerClass);
 		}
-		//=================================end
+		// =================================end
 
 		log.error(msg);
 	}
-	
-	public static void error(String msg,Throwable t) {
 
-		//=================================start
+	public static void error(String msg, Throwable t) {
+
+		// =================================start
 		if (LoggerFactory.isNoArgInConstructor()) {
 			resetLog();
 		} else {
-			//不能移走,它表示的是调用所在的位置.
+			// 不能移走,它表示的是调用所在的位置.
 			String callerClass = "";
 			try {
 				callerClass = sun.reflect.Reflection.getCallerClass().getName();
@@ -403,18 +403,18 @@ public class Logger {
 			}
 			resetLog(callerClass);
 		}
-		//=================================end
+		// =================================end
 
-		log.error(msg,t);
+		log.error(msg, t);
 	}
 
 	public static void error(Number msg) {
 
-		//=================================start
+		// =================================start
 		if (LoggerFactory.isNoArgInConstructor()) {
 			resetLog();
 		} else {
-			//不能移走,它表示的是调用所在的位置.
+			// 不能移走,它表示的是调用所在的位置.
 			String callerClass = "";
 			try {
 				callerClass = sun.reflect.Reflection.getCallerClass().getName();
@@ -431,21 +431,21 @@ public class Logger {
 			}
 			resetLog(callerClass);
 		}
-		//=================================end
+		// =================================end
 
 		log.error(msg + "");
 	}
-	
+
 	private static void resetLog() {
 		if (LoggerFactory.isNoArgInConstructor()) {
-			log = _getLog(); //Log4jImpl,Slf4jImpl,SystemLogger,NoLogging,FileLogger 可以不需要参数
-		} 
+			log = _getLog(); // Log4jImpl,Slf4jImpl,SystemLogger,NoLogging,FileLogger 可以不需要参数
+		}
 	}
 
 	private static void resetLog(String callerClass) {
 
 		if (LoggerFactory.isNoArgInConstructor()) {
-			log = _getLog(); //log4j   要解决同时处理两个构造函数
+			log = _getLog(); // log4j 要解决同时处理两个构造函数
 		} else {
 			try {
 				log = _getLog(callerClass);
@@ -454,7 +454,7 @@ public class Logger {
 			}
 		}
 	}
-	
+
 	private static Log _getLog() {
 		return LoggerFactory.getLog();
 	}
